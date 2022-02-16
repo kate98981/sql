8fa64c2933c63aaf1e4a53307b77676b39835130
kate98981 <61939550+kate98981@users.noreply.github.com>
2022-02-16 12:29:48 +0500

    Update SQL_HW_1.sql

--- SQL_HW_1.sql
+++ SQL_HW_1.sql
@@ -27,6 +27,7 @@ select * from students where created_on = '2021-03-26 00:00:00';
 
 -- 10.Вывести пользователей где в имени есть слово Анна
 select * from students where name like '%Anna%';
+select * from students where name like 'Anna%';
 
 -- 11.Вывести пользователей где в имени в конце есть 8
 select * from students where name like '%8';
